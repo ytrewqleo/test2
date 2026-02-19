a618bac96f9f0e405708ac3984b3f14207088861
admin <leo.vokra@gmail.com>
2026-02-19 20:21:39 +0300

    add README.md

---
+++ README.md
@@ -0,0 +1,6 @@
+#example
+
+*first
+*second
+
+**Bold**
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "gello" << '\n';
+    std::cout << "fichA" << '\n';
+    std::cout << "work" << '\n';
 }
